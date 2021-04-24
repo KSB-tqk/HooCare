15cdd9c1c9a5a0633ea8044bade26fbd9036e9ca
Khánh Trần Quốc <19521686@ms.uit.edu.vn>
2021-04-24 11:19:12 +0700

    Update UI Meal Fragment(clean code and add class MealPlanData into Meal_Plan_RecyclerView)

--- app/src/main/java/cf/khanhsb/icare_v2/MealPlanData.java
+++ app/src/main/java/cf/khanhsb/icare_v2/MealPlanData.java
@@ -11,6 +11,15 @@ public class MealPlanData {
     private ArrayList<Integer> icons;
     private ArrayList<Drawable> backgrounds;
 
+    public MealPlanData(ArrayList<String> titles,ArrayList<String> detailOfMeal,
+                        ArrayList<String> kcal, ArrayList<Integer> icons,
+                        ArrayList<Drawable> backgrounds){
+        this.detailOfMeal = detailOfMeal;
+        this.kcal = kcal;
+        this.titles = titles;
+        this.icons = icons;
+        this.backgrounds = backgrounds;
+    }
     public void setIcons(ArrayList<Integer> icons) {
         this.icons = icons;
     }
--- app/src/main/java/cf/khanhsb/icare_v2/MealViewPagerAdapter.java
+++ app/src/main/java/cf/khanhsb/icare_v2/MealViewPagerAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,18 +14,12 @@ import java.util.ArrayList;
 
 public class MealViewPagerAdapter extends RecyclerView.Adapter {
     //initialize variable
-    private ArrayList<String> mealItemTitles,detailOfMealText,numberOfKcalText;
-    private ArrayList<Integer> images;
+    private MealPlanData mealPlanData;
 
 
     //create constructor
-    public MealViewPagerAdapter(ArrayList<String> mealsTitles,
-                                ArrayList<String> detailOfMealText,
-                                ArrayList<String> numberOfKcalText){
-        this.mealItemTitles = mealsTitles;
-        this.detailOfMealText = detailOfMealText;
-        this.numberOfKcalText = numberOfKcalText;
-
+    public MealViewPagerAdapter(MealPlanData mealPlanData){
+        this.mealPlanData = mealPlanData;
     }
 
     @NonNull
@@ -41,15 +36,17 @@ public class MealViewPagerAdapter extends RecyclerView.Adapter {
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         ViewHolderClass viewHolderClass = (ViewHolderClass) holder;
         //error
-        viewHolderClass.titles.setText(mealItemTitles.get(position));
-        viewHolderClass.detailOfMeal.setText(detailOfMealText.get(position));
-        viewHolderClass.numberOfKcal.setText(numberOfKcalText.get(position));
+        viewHolderClass.titles.setText(mealPlanData.getTitles().get(position));
+        viewHolderClass.detailOfMeal.setText(mealPlanData.getDetailOfMeal().get(position));
+        viewHolderClass.numberOfKcal.setText(mealPlanData.getKcal().get(position));
+        viewHolderClass.imageView.setImageResource(mealPlanData.getIcons().get(position));
+        viewHolderClass.backgroundView.setBackground(mealPlanData.getBackgrounds().get(position));
 
     }
 
     @Override
     public int getItemCount() {
-        return mealItemTitles.size();
+        return mealPlanData.getTitles().size();
     }
 
     public class ViewHolderClass extends RecyclerView.ViewHolder {
@@ -57,11 +54,13 @@ public class MealViewPagerAdapter extends RecyclerView.Adapter {
         //initialize variable in the item container
         ImageView imageView;
         TextView titles,numberOfKcal,detailOfMeal;
+        LinearLayout backgroundView;
 
         public ViewHolderClass(@NonNull View itemView) {
             super(itemView);
 
             imageView = itemView.findViewById(R.id.meal_icon);
+            backgroundView = itemView.findViewById(R.id.meal_detail_background_view);
             titles = itemView.findViewById(R.id.meal_title);
             detailOfMeal = itemView.findViewById(R.id.meal_detail);
             numberOfKcal = itemView.findViewById(R.id.numberOfKcal_MealFrag_ViewPager);
