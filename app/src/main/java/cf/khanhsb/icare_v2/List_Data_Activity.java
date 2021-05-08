39f6ff631affa8ed23ce05c128a6dcff7227c46a
Khánh Trần Quốc <19521686@ms.uit.edu.vn>
2021-05-08 11:02:14 +0700

    Update GymFragment UI

--- app/src/main/java/cf/khanhsb/icare_v2/List_Data_Activity.java
+++ app/src/main/java/cf/khanhsb/icare_v2/List_Data_Activity.java
@@ -6,11 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class List_Data_Activity extends AppCompatActivity {
     private ImageView workoutImage,backButton,moreButton,favButton;
-    private TextView workoutTitle;
+    private TextView workoutTitle,workoutTime;
+
+    /**animation exercise listview*/
+    private ListView listView;
+    private String[] anim_exer_ListTitle = {"JUMPING JACKS",
+            "ABSDOMINAL CRUNCHES",
+            "RUSSIAN TWIST",
+            "MOUNTAIN CLIMBER",
+            "HEEL TOUCH"};
+    private int[] gymListImage = {R.drawable.jumping_jack_list_data_item,
+            R.drawable.jumping_jack_list_data_item,
+            R.drawable.jumping_jack_list_data_item,
+            R.drawable.jumping_jack_list_data_item,
+            R.drawable.jumping_jack_list_data_item};
+    /**animation exercise listview*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +50,10 @@ public class List_Data_Activity extends AppCompatActivity {
 
         workoutTitle.setText(intent.getStringExtra("workoutTitle"));
         workoutImage.setImageResource(intent.getIntExtra("workoutImage",0));
+
+        /**setting up animation exercise listview*/
+        listView = findViewById(R.id.list_view_list_data);
+
+
     }
 }
