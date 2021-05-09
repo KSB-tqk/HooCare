40fbd373bba45e3e11a5fb2a432b8fbb4ae4dc49
Khánh Trần Quốc <19521686@ms.uit.edu.vn>
2021-05-09 19:54:23 +0700

    Update GymFragment UI

--- app/src/main/java/cf/khanhsb/icare_v2/List_Data_Activity.java
+++ app/src/main/java/cf/khanhsb/icare_v2/List_Data_Activity.java
@@ -1,22 +1,34 @@
 package cf.khanhsb.icare_v2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.ListView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import java.util.Objects;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class List_Data_Activity extends AppCompatActivity {
-    private ImageView workoutImage,backButton,moreButton,favButton;
-    private TextView workoutTitle,workoutTime;
+    private ImageView workoutImage, backButton, moreButton, favButton;
+    private TextView workoutTitle, workoutTime, exerciseTitle, exerciseDurationValue,
+            exerciseDurationText,animTitle,videoTitle;
+    private LinearLayout startButtonAnimExer;
+    private ConstraintLayout bottomSheetContainer,selectedBackground;
+    private ViewPager2 viewPager2;
 
-    /**animation exercise listview*/
+    /**
+     * animation exercise listview
+     */
     private NonScrollListView listView;
     private String[] anim_exer_ListTitle = {"JUMPING JACKS",
             "ABSDOMINAL CRUNCHES",
@@ -28,12 +40,25 @@ public class List_Data_Activity extends AppCompatActivity {
             "x20",
             "x20",
             "x20"};
+    private String[] anim_exer_ListDurationText = {"Duration (Seconds)",
+            "Duration (Reps)",
+            "Duration (Reps)",
+            "Duration (Reps)",
+            "Duration (Reps)"};
+    private String[] anim_exer_ListDurationValue = {"20",
+            "16",
+            "20",
+            "20",
+            "20"};
     private int[] gymListImage = {R.drawable.jumping_jack_list_data_item,
             R.drawable.jumping_jack_list_data_item,
             R.drawable.jumping_jack_list_data_item,
             R.drawable.jumping_jack_list_data_item,
             R.drawable.jumping_jack_list_data_item};
-    /**animation exercise listview*/
+
+    /**
+     * animation exercise listview
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +68,16 @@ public class List_Data_Activity extends AppCompatActivity {
         workoutTitle = (TextView) findViewById(R.id.gym_list_workout_text);
         workoutImage = (ImageView) findViewById(R.id.gym_list_image_view);
         backButton = (ImageView) findViewById(R.id.back_button_list_data);
+        bottomSheetContainer = (ConstraintLayout) findViewById(R.id.bottom_sheet_container_exer_anim);
+        startButtonAnimExer = (LinearLayout) findViewById(R.id.start_button_anim_exer_linear);
+
+        startButtonAnimExer.bringToFront();
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(List_Data_Activity.this,MainActivity.class);
-                intent.putExtra("fragmentPosition",3);
+                Intent intent = new Intent(List_Data_Activity.this, MainActivity.class);
+                intent.putExtra("fragmentPosition", 3);
                 startActivity(intent);
                 overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
             }
@@ -57,23 +86,65 @@ public class List_Data_Activity extends AppCompatActivity {
         Intent intent = getIntent();
 
         workoutTitle.setText(intent.getStringExtra("workoutTitle"));
-        workoutImage.setImageResource(intent.getIntExtra("workoutImage",0));
+        workoutImage.setImageResource(intent.getIntExtra("workoutImage", 0));
 
         /**setting up animation exercise listview*/
         listView = findViewById(R.id.list_view_list_data);
         AnimExerListViewAdapter animExerListViewAdapter = new AnimExerListViewAdapter(this,
-                R.layout.item_exercises_list_data,gymListImage,anim_exer_ListTitle,anim_exer_ListText);
+                R.layout.item_exercises_list_data, gymListImage, anim_exer_ListTitle, anim_exer_ListText);
         listView.setAdapter(animExerListViewAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(List_Data_Activity.this,List_Exercises_Activity.class);
-                intent.putExtra("exerciseTitle",anim_exer_ListTitle[position]);
-                intent.putExtra("exerciseDuration",anim_exer_ListText[position]);
-                startActivity(intent);
-                overridePendingTransition(R.anim.slide_in_right, R.anim.hold_position);
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+                        List_Data_Activity.this, R.style.BottomSheetDialogTheme);
+                View bottomSheetView = LayoutInflater.from(getApplicationContext())
+                        .inflate(
+                                R.layout.bottom_sheet_dialog_anim_exer,
+                                bottomSheetContainer,
+                                false
+                        );
+                exerciseTitle = bottomSheetView.findViewById(R.id.title_animation_exercise);
+                animTitle = bottomSheetView.findViewById(R.id.animation_title);
+                videoTitle = bottomSheetView.findViewById(R.id.video_title);
+                selectedBackground =  bottomSheetView.findViewById(R.id.tab_animation_view);
+
+                videoTitle.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int size = videoTitle.getWidth();
+                        videoTitle.setTypeface(videoTitle.getTypeface(), Typeface.BOLD);
+                        animTitle.setTypeface(null, Typeface.NORMAL);
+                        selectedBackground.animate().x(size).setDuration(200);
+                    }
+                });
+
+                animTitle.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        videoTitle.setTypeface(null, Typeface.NORMAL);
+                        animTitle.setTypeface(animTitle.getTypeface(), Typeface.BOLD);
+                        selectedBackground.animate().x(0).setDuration(200);
+                    }
+                });
+                exerciseTitle.setText(anim_exer_ListTitle[position]);
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
             }
         });
 
+        /**app bar animation*/
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.gym_list_app_bar);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (Math.abs(verticalOffset) == appBarLayout.getTotalScrollRange()) {
+                    workoutTitle.setAlpha(1f);
+                } else if (verticalOffset == 0) {
+                    workoutTitle.setAlpha(0f);
+                } else {
+                }
+            }
+        });
     }
 }
