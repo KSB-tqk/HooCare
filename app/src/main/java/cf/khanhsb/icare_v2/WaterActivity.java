43cf6f06db36b40749675c39b4a50010ed548418
Khánh Trần Quốc <19521686@ms.uit.edu.vn>
2021-05-13 10:53:28 +0700

    Update WaterActivity UI

--- app/src/main/java/cf/khanhsb/icare_v2/WaterActivity.java
+++ app/src/main/java/cf/khanhsb/icare_v2/WaterActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -17,6 +18,7 @@ public class WaterActivity extends AppCompatActivity {
     private int waterHasDrink = 0;
     private WaveView waveView;
     private SeekBar seekBar;
+    private ImageView backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +26,17 @@ public class WaterActivity extends AppCompatActivity {
 
         seekBar = findViewById(R.id.water_seek_bar);
         waveView = findViewById(R.id.wave_view_water);
+        backButton = findViewById(R.id.button_backtohomefrag);
+
+        backButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toMain = new Intent(WaterActivity.this,MainActivity.class);
+                startActivity(toMain);
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+            }
+        });
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
