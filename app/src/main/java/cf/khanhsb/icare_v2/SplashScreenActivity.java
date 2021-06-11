3a63cbf34748266944c1d2bdefc760258f6361ee
The Dan <19521312@gm.uit.edu.vn>
2021-06-11 23:53:23 +0700

    GoogleLogin, RememberUser

--- app/src/main/java/cf/khanhsb/icare_v2/SplashScreenActivity.java
+++ app/src/main/java/cf/khanhsb/icare_v2/SplashScreenActivity.java
@@ -6,20 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class SplashScreenActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class SplashScreenActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     private static final int SPLASH_SCREEN = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                if(user != null){
+                    Intent mainintent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                    startActivity(mainintent);
+                    finish();
+                }else{
                 Intent intent = new Intent(SplashScreenActivity.this,OnBoardActivity.class);
                 startActivity(intent);
-                finish();
+                finish();}
             }
         },SPLASH_SCREEN);
     }
