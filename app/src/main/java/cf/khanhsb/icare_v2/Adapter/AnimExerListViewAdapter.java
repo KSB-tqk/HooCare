fcf796f8e86483331c1c5cf24ec64f10b1ec5a43
KSB-tqk <linh05602@gmail.com>
2021-06-27 23:04:26 +0700

    fix GymFragment

--- app/src/main/java/cf/khanhsb/icare_v2/Adapter/AnimExerListViewAdapter.java
+++ app/src/main/java/cf/khanhsb/icare_v2/Adapter/AnimExerListViewAdapter.java
@@ -2,6 +2,7 @@ package cf.khanhsb.icare_v2.Adapter;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.util.Log;
@@ -15,10 +16,15 @@ import android.widget.TextView;
 import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
@@ -78,6 +84,7 @@ public class AnimExerListViewAdapter extends BaseAdapter {
         holder.exerciseText = (TextView) view.findViewById(R.id.exercises_workout_duration);
         holder.exerciseGif = (ImageView) view.findViewById(R.id.exercises_video);
         holder.frameLayout = (FrameLayout) view.findViewById(R.id.exercises_video_frame);
+        holder.loadingIcon = (ImageView) view.findViewById(R.id.loading_image_list_data);
 
         holder.exerciseTitle.setText(title.get(position));
         holder.exerciseText.setText(textDetail.get(position));
@@ -85,6 +92,19 @@ public class AnimExerListViewAdapter extends BaseAdapter {
         Glide.with(context)
                 .load(videoUri.get(position))
                 .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        holder.exerciseGif.setVisibility(View.VISIBLE);
+                        holder.loadingIcon.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
                 .into(holder.exerciseGif);
 
 
@@ -93,7 +113,7 @@ public class AnimExerListViewAdapter extends BaseAdapter {
     public class Holder
     {
         TextView exerciseTitle,exerciseText;
-        ImageView exerciseGif;
+        ImageView exerciseGif,loadingIcon;
         FrameLayout frameLayout;
     }
 }
